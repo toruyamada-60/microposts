b246c7b1a9a2554014289f216ac40e932b8c2192
toru.yamada <1194595t@gmail.com>
2019-11-17 04:20:10 +0000

    add favorite

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -20,7 +20,6 @@ class MicropostsController < ApplicationController
     @micropost.destroy
     flash[:success] = 'メッセージを削除しました。'
     redirect_back(fallback_location: root_path)
-    #redirect_to root_url
   end
   
   private
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   
   validates :content, presence: true, length: { maximum: 255 }
+  
+  has_many :favorites, foreign_key: 'micropost_id'
+  has_many :favusers, through: :favotrites, source: :user
 end
